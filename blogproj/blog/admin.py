a9c6fab561bc0e2a9f09603275e4cde8beef4ef8
Qiyue <zhaosld@yahoo.com>
2017-07-16 12:16:08 -0700

    first commit

---
+++ blogproj/blog/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from blog.models import Post,Comment,Project
+# Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Project)
