e1237716ae43f0a46bd3c5e64534e9d17d60d08a
Qiyue <zhaosld@yahoo.com>
2017-07-17 00:00:56 -0700

    update css

--- blogproj/blogproj/settings.py
+++ blogproj/blogproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tv*ypk&9=jhgvgseb(i(ca+)%m6!3w@9^y-0uv7&4kkezi3-rg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.qzite.club']
 
 
 # Application definition
